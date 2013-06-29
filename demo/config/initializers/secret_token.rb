a72c6fb0b437033460e72e8810eb8144fd6b2040
ahmedkamran <kamranseacom@gmail.com>
2013-06-29 23:46:13 +0530

    new model,all task done

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '73df32281bb715d9ab990cc88118960ff5d1fc4ccbdc50f1d4b40041c7c8a93f785471be6414d57742356eb95653214d9ef73faaa3537a83c1c3086b55e2d73b'
---
+++ demo/db/migrate/20130629180857_add_lastname_to_users.rb
@@ -0,0 +1,6 @@
+class AddLastnameToUsers < ActiveRecord::Migration
+  def change
+	add_column :users, :lastname, :string
+	rename_column :users, :username, :firstname
+  end
+end
